0b3132222348ea555fafb1b9cfd601c72d55578b
SplitPersona <stephenck0203@gmail.com>
2024-02-10 16:22:55 +0000

    Question 7 finished

--- src/CA3_Question7.java
+++ src/CA3_Question7.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.util.Scanner;
 import java.util.Queue;
 import java.util.LinkedList;
+
 /**
  *  Name: Stephen Carragher Kelly
  *  Class Group: GD2A
@@ -21,8 +22,58 @@ class Block2
 
 public class CA3_Question7
 {
+    private static Map<String, Queue<Block>> sharesMap = new HashMap<>();
 
-    
+    public static void buy(String company, int quantity, double price)
+    {
+        sharesMap.putIfAbsent(company, new LinkedList<>());
+        Queue<Block> sharesQueue = sharesMap.get(company);
+        sharesQueue.add(new Block(quantity, price));
+       // Block block = new Block(quantity, price);
+       // shares.add(block);
+    }
+
+    public static void sell(String company, int quantity)
+    {
+        double totalgain = 0;
+        Queue<Block> sharesQueue = sharesMap.get(company);
+        if(sharesQueue != null)
+        {
+            while(quantity > 0 && !sharesQueue.isEmpty())
+            {
+                Block block = sharesQueue.poll();
+                int blockquantity = block.quantity;
+
+                if(blockquantity <= quantity)
+                {
+                    totalgain += (quantity - blockquantity) * block.price;
+                    quantity -= blockquantity;
+                }
+                else
+                {
+                    totalgain += quantity * block.price;
+                    block.quantity -= quantity;
+                    sharesQueue.add(block);
+                    quantity = 0;
+                }
+
+            }
+            if(quantity > 0)
+            {
+                System.out.println("No shares in "+company+" to sell");
+            }
+            else
+            {
+                System.out.println("Total Gain for "+company+": "+totalgain);
+            }
+        }
+        else
+        {
+            System.out.println("No shares for this company");
+        }
+
+    }
+
 
     /*
    Will repeatedly ask the user to enter the commands in the format
