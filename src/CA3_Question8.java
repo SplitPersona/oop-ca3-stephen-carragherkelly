7e954e4ca074ff852edee7d3b0fa4f97ba38273b
SplitPersona <stephenck0203@gmail.com>
2024-02-11 14:18:04 +0000

    Questions 8 & 9 finished

--- src/CA3_Question8.java
+++ src/CA3_Question8.java
@@ -1,9 +1,99 @@
 import java.util.Scanner;
+import java.util.Stack;
 /**
- *  Name:
- *  Class Group:
+ *  Name: Stephen Carragher Kelly
+ *  Class Group: GD2A
  */
 public class CA3_Question8 {
+
+    public static double checkexpression(String expression)
+    {
+        Stack<Double> oprandstack = new Stack<>();
+        Stack<Character> operatorstack = new Stack<>();
+
+        for(char ch:expression.toCharArray())
+        {
+            if(Character.isDigit(ch))
+            {
+                oprandstack.push((double)(ch - '0'));
+
+            }
+            else if(ch == '(')
+            {
+                operatorstack.push(ch);
+            }
+            else if(ch == ')')
+            {
+                while(!operatorstack.isEmpty() && operatorstack.peek() != '(')
+                {
+                    domaths(oprandstack, operatorstack);
+                }
+                operatorstack.pop();
+            }
+            else if(isoperator(ch))
+            {
+                while(!operatorstack.isEmpty() && hasprecedence(ch, operatorstack.peek()))
+                {
+                    domaths(oprandstack, operatorstack);
+                }
+                operatorstack.push(ch);
+            }
+        }
+        while(!operatorstack.isEmpty())
+        {
+            domaths(oprandstack, operatorstack);
+        }
+
+        return oprandstack.pop();
+    }
+
+    private static void domaths(Stack<Double> oprandstack, Stack<Character> operatorstack)
+    {
+        char operation = operatorstack.pop();
+        double oprand2 = oprandstack.pop();
+        double oprand1 = oprandstack.pop();
+
+        switch(operation)
+        {
+            case '+':
+                oprandstack.push(oprand1 + oprand2);
+                break;
+            case '-':
+                oprandstack.push(oprand1 - oprand2);
+                break;
+            case '*':
+                oprandstack.push(oprand1 * oprand2);
+                break;
+            case '/':
+                oprandstack.push(oprand1 / oprand2);
+                break;
+        }
+    }
+
+    private static boolean isoperator(char ch)
+    {
+        return ch == '+' || ch == '-' || ch == '*' || ch == '/';
+    }
+
+    private static int precedeure(char operator)
+    {
+        if(operator == '+' || operator == '-')
+        {
+            return 1;
+        }
+        else if (operator == '*' || operator == '/')
+        {
+            return 2;
+        }
+
+        return 0;
+    }
+
+    private static boolean hasprecedence(char op1, char op2)
+    {
+        return precedeure(op1) >= precedeure(op2);
+    }
+
 
     /*
         Reads in an equation from the user
@@ -13,5 +103,7 @@ public class CA3_Question8 {
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter equation");
         equation = in.nextLine().trim();
+        double result = checkexpression(equation);
+        System.out.println("Result: " + result);
     }
 }
--- src/CA3_Question9.java
+++ src/CA3_Question9.java
@@ -1,6 +1,8 @@
+import java.util.Stack;
+
 /**
- *  Name:
- *  Class Group:
+ *  Name: Stephen Carragher Kelly
+ *  Class Group: GD2A
  */
 
 /*
@@ -21,8 +23,67 @@ public class CA3_Question9
             System.out.println();
         }
     }
-    public void solve(int x, int y, DIRECTION dir)
+    public void solve(int[][] maze, int startx, int starty, int exitx, int exity)
     {
+        Stack<int[]> path = new Stack<>();
+        path.push(new int[]{startx, starty});
+        while(!path.isEmpty())
+        {
+            int[] currentposition = path.pop();
+            int x = currentposition[0];
+            int y = currentposition[1];
+
+            if(x==exitx && y==exity)
+            {
+                System.out.println("You have found the exist");
+                return;
+            }
+
+            for(DIRECTION dir: DIRECTION.values())
+            {
+                int newx = x;
+                int newy = y;
+                switch(dir)
+                {
+                    case NORTH:
+                        newx--;
+                        break;
+                    case SOUTH:
+                        newx++;
+                        break;
+                    case EAST:
+                        newy++;
+                        break;
+                    case WEST:
+                        newy--;
+                        break;
+                }
+
+                if(newx >= 0 && newx < maze.length && newy >= 0 && newy < maze[0].length && maze[newx][newy] == 0)
+                {
+                    path.push(new int[]{newx, newy});
+                    maze[newx][newy] = 2;
+                }
+
+            }
+        }
+
+        System.out.println("No path their");
 
     }
+
+    public static void main(String[] args)
+    {
+        int[][] maze = {
+                {1, 1, 1, 1, 1},
+                {1, 0, 0, 0, 1},
+                {1, 1, 1, 0, 1},
+                {1, 0, 0, 0, 1},
+                {1, 1, 1, 1, 1}
+        };
+        CA3_Question9 mazeSolver = new CA3_Question9();
+        mazeSolver.solve(maze, 1, 1, 3, 3);
+        display(maze);
+    }
+
 }
